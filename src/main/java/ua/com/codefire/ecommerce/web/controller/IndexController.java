1f1f99c13e4edae9764713e80131881aaeacf00e
ankys <andr.kisilevskiy@gmail.com>
2017-02-09 05:32:50 +0300

    Added json to project. Add libraries to your classpath

--- src/main/java/ua/com/codefire/ecommerce/web/controller/IndexController.java
+++ src/main/java/ua/com/codefire/ecommerce/web/controller/IndexController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ua.com.codefire.ecommerce.data.entity.*;
 import ua.com.codefire.ecommerce.data.entity.Currency;
 import ua.com.codefire.ecommerce.data.repo.*;
@@ -179,6 +176,7 @@ public class IndexController {
         });
     }
 
+    @ResponseBody
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String index(@RequestParam Integer pageNumber, @RequestParam Integer amountPerPage, Model model) {
         List<Product> allProducts = productService.getProductsByPage(pageNumber, amountPerPage);
