7dcc2de59004150f68baf3a0090f335b5b09a47d
va13ak <va13ak@gmail.com>
2017-02-10 11:17:20 +0200

    merged with movlyan and fix

--- src/main/java/ua/com/codefire/ecommerce/web/controller/FilterController.java
+++ src/main/java/ua/com/codefire/ecommerce/web/controller/FilterController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ua.com.codefire.ecommerce.data.entity.Product;
 import ua.com.codefire.ecommerce.data.entity.ProductType;
 import ua.com.codefire.ecommerce.data.service.ProductService;
