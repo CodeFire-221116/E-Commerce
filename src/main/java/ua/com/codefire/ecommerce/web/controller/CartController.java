e2f2886c7e673862d36f813b0d08045d0378a7f8
va13ak <va13ak@gmail.com>
2017-02-16 10:44:53 +0200

    changed map to shopping cart in cart controller

--- src/main/java/ua/com/codefire/ecommerce/web/controller/CartController.java
+++ src/main/java/ua/com/codefire/ecommerce/web/controller/CartController.java
@@ -7,15 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import ua.com.codefire.ecommerce.data.entity.Product;
+import ua.com.codefire.ecommerce.data.entity.ShoppingCart;
 import ua.com.codefire.ecommerce.data.service.ProductService;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.Parameter;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Valery on 15.02.2017.
@@ -27,39 +23,36 @@ public class CartController
     @Autowired
     private ProductService productService;
 
-    private Map<Product, Integer> getCartContent() {
+    private ShoppingCart getCartContent() {
         ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
 
         return getCartContent(attr.getRequest());
     }
 
-    private Map<Product, Integer> getCartContent(HttpServletRequest request) {
+    private ShoppingCart getCartContent(HttpServletRequest request) {
         HttpSession session = request.getSession();
 
-        Map<Product, Integer> cart = (Map<Product, Integer>) session.getAttribute("cart");
-        if (cart == null) {
-            cart = new HashMap<>();
-            session.setAttribute("cart", cart);
+        ShoppingCart shoppingCart = (ShoppingCart) session.getAttribute("shoppingCart");
+        if (shoppingCart == null) {
+            shoppingCart = new ShoppingCart();
+            session.setAttribute("shoppingCart", shoppingCart);
         }
 
-        return cart;
+        return shoppingCart;
     }
 
 
     @RequestMapping("/cart")
     public String getCart(Model model) {
-        model.addAttribute("cartContent", getCartContent());
+        model.addAttribute("shoppingCart", getCartContent());
 
         return "/products/cart";
     }
 
     @RequestMapping("/cart/add")
     public String addToCart(@RequestParam int productId, HttpServletRequest request, Model model) {
-        Map<Product, Integer> cart = getCartContent(request);
-        Product product = productService.getProduct(productId);
 
-        int count = cart.getOrDefault(product, 0);
-        cart.put(product, count + 1);
+        getCartContent(request).addProductToCart(productService.getProduct(productId));
 
         String referer = request.getHeader("Referer");
         return "redirect:" + referer;
@@ -67,9 +60,7 @@ public class CartController
 
     @RequestMapping("/cart/remove")
     public String removeFromCart(@RequestParam int productId, Model model) {
-        Map<Product, Integer> cart = getCartContent();
-
-        cart.remove(productService.getProduct(productId));
+        getCartContent().removeProductFromCart(productService.getProduct(productId));
 
         return "redirect:/cart";
     }
