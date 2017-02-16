1daad64c5cc340acc5ea3e81725b3045da6edfed
va13ak <va13ak@gmail.com>
2017-02-16 11:52:25 +0200

    cart controller to web_controllers moved

--- src/main/java/ua/com/codefire/ecommerce/web/controller/web_controllers/CartController.java
+++ src/main/java/ua/com/codefire/ecommerce/web/controller/web_controllers/CartController.java
@@ -1,4 +1,4 @@
-package ua.com.codefire.ecommerce.web.controller;
+package ua.com.codefire.ecommerce.web.controller.web_controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
